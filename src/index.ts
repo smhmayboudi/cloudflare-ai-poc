dc6f42f9d1e751bf116c8658bb6e39b6773c33cf
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-03-27 09:34:27 +0330

    feat: add weather

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ import { autoTrimTools, createToolsFromOpenAPISpec, runWithTools } from '@cloudf
 
 export interface Env {
 	AI: Ai;
+	WEATHERAPI_TOKEN: string;
 }
 
 export default {
@@ -67,43 +68,87 @@ export default {
 
 		// return Response.json(result.object);
 
+		// # 4 Test
+		// const response = await runWithTools(
+		// 	env.AI as unknown as import('@cloudflare/workers-types').Ai,
+		// 	'@hf/nousresearch/hermes-2-pro-mistral-7b',
+		// 	{
+		// 		messages: [{ role: 'user', content: 'Who is Cloudflare on github?' }],
+		// 		tools: [
+		// 			// You can pass the OpenAPI spec link or contents directly
+		// 			...(await createToolsFromOpenAPISpec(
+		// 				'https://gist.githubusercontent.com/mchenco/fd8f20c8f06d50af40b94b0671273dc1/raw/f9d4b5cd5944cc32d6b34cad0406d96fd3acaca6/partial_api.github.com.json',
+		// 				{
+		// 					overrides: [
+		// 						{
+		// 							// for all requests on *.github.com, we'll need to add a User-Agent.
+		// 							matcher: ({ url, method }) => {
+		// 								return url.hostname === 'api.github.com';
+		// 							},
+		// 							values: {
+		// 								headers: {
+		// 									'User-Agent':
+		// 										'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+		// 								},
+		// 							},
+		// 						},
+		// 					],
+		// 				}
+		// 			)),
+		// 		],
+		// 	},
+		// 	{
+		// 		// maxRecursiveToolRuns: 0,
+		// 		// streamFinalResponse: true,
+		// 		// strictValidation: true,
+		// 		trimFunction: autoTrimTools,
+		// 		verbose: true,
+		// 	}
+		// );
+
 		const response = await runWithTools(
 			env.AI as unknown as import('@cloudflare/workers-types').Ai,
 			'@hf/nousresearch/hermes-2-pro-mistral-7b',
 			{
-				messages: [{ role: 'user', content: 'Who is Cloudflare on github?' }],
+				messages: [{ role: 'user', content: "What's the weather in Austin, Texas?" }],
 				tools: [
-					// You can pass the OpenAPI spec link or contents directly
-					...(await createToolsFromOpenAPISpec(
-						'https://gist.githubusercontent.com/mchenco/fd8f20c8f06d50af40b94b0671273dc1/raw/f9d4b5cd5944cc32d6b34cad0406d96fd3acaca6/partial_api.github.com.json',
-						{
-							overrides: [
-								{
-									// for all requests on *.github.com, we'll need to add a User-Agent.
-									matcher: ({ url, method }) => {
-										return url.hostname === 'api.github.com';
-									},
-									values: {
-										headers: {
-											'User-Agent':
-												'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-										},
-									},
+					{
+						description: 'Return the weather for a latitude and longitude',
+						function: async ({ latitude, longitude }) => {
+							const url = `https://api.weatherapi.com/v1/current.json?key=${env.WEATHERAPI_TOKEN}&q=${latitude},${longitude}`;
+							const res = await fetch(url).then((res) => res.json());
+
+							return JSON.stringify(res);
+						},
+						name: 'getWeather',
+						parameters: {
+							properties: {
+								latitude: {
+									description: 'The latitude for the given location',
+									type: 'string',
 								},
-							],
-						}
-					)),
+								longitude: {
+									description: 'The longitude for the given location',
+									type: 'string',
+								},
+							},
+							required: ['latitude', 'longitude'],
+							type: 'object',
+						},
+					},
 				],
 			},
 			{
-				// maxRecursiveToolRuns: 0,
-				// streamFinalResponse: true,
-				// strictValidation: true,
+				maxRecursiveToolRuns: 5,
+				streamFinalResponse: true,
+				strictValidation: true,
 				trimFunction: autoTrimTools,
 				verbose: true,
 			}
 		);
 
 		return new Response(JSON.stringify(response));
+
+		// https://raw.githubusercontent.com/github/rest-api-description/main/descriptions-next/api.github.com/api.github.com.json
 	},
 } satisfies ExportedHandler<Env>;
